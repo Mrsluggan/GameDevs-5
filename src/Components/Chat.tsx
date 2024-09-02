58d9347d5381162655a19ac60710918cee716d6c
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-09-02 13:47:25 +0200

    deploy

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -1,23 +1,12 @@
-import React from 'react'
-import './Chat.css';
+import "./Chat.css";
 
 export default function Chat() {
-
-
-
-
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column',marginLeft: '5%'}}>
-            <div className="chat-container">
-
-
-                hello
-
-
-            </div>
-            <div >
-                <input type="text" style={{width: '100%', height: '40px'}} />
-            </div>
-        </div>
-    )
+  return (
+    <div style={{ display: "flex", flexDirection: "column", marginLeft: "5%" }}>
+      <div className="chat-container">hello</div>
+      <div>
+        <input type="text" style={{ width: "100%", height: "40px" }} />
+      </div>
+    </div>
+  );
 }
