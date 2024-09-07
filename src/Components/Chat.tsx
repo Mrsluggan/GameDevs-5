ba196d3325256df2776f2ea634367dc64a88dbed
mrsluggan <ericflyger@gmail.com>
2024-09-07 11:04:22 +0200

    asdasda

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -2,26 +2,31 @@ import { useEffect, useState } from "react";
 import { useStompClient, useSubscription } from "react-stomp-hooks";
 interface Props {
   gameRoomID: string
+
 }
 export default function Chat({ gameRoomID }: Props) {
+
   const stompClient = useStompClient();
   const [output, setoutput] = useState<any>("");
-  const [listOfMessages, setListOfMessages] = useState<any>([])
+  const [message, setMessage] = useState<any>("");
 
+
+  const [listOfMessages, setListOfMessages] = useState<any[]>([]);
 
   useSubscription("/topic/welcome/" + gameRoomID, (message: any) => {
-    let data = JSON.parse(message.body);
-    console.log(data.message);
-    setoutput(data.message);
-    listOfMessages.push(data.message);
-    setListOfMessages(listOfMessages);
 
+    let parsed = JSON.parse(message.body);
+
+    console.log(parsed);
+
+    setListOfMessages((prevMessages: any[]) => [...prevMessages, parsed]);
   });
 
   useSubscription("/topic/message/" + gameRoomID, (message: any) => {
-    console.log(message.body);
+    let parsed = JSON.parse(message.body);
+    console.log(parsed);
 
-
+    setListOfMessages((prevMessages: string[]) => [...prevMessages, parsed]);
   });
 
   const sendWelcome = () => {
@@ -30,10 +35,14 @@ export default function Chat({ gameRoomID }: Props) {
     let username = localStorage.getItem("username")!;
 
     if (stompClient) {
+      console.log(username);
+
       //Send Message
       stompClient.publish({
         destination: "/app/welcome/" + gameRoomID,
-        body: JSON.stringify({ username: username }),
+        body: JSON.stringify({
+          username: username
+        }),
       });
     } else {
       //Handle error
@@ -41,17 +50,34 @@ export default function Chat({ gameRoomID }: Props) {
   }
 
   const sendMessage = (message: string) => {
+    console.log("skickar meddelande");
+    console.log(message);
 
     if (stompClient) {
       //Send Message
       stompClient.publish({
         destination: "/app/message/" + gameRoomID,
-        body: JSON.stringify({ message: message }),
+        body: JSON.stringify({
+          content: message,
+          sender: localStorage.getItem("username")
+        }),
       });
     } else {
       //Handle error
     }
   }
+
+  const loadMessags = () => {
+
+    fetch("http://localhost:8080/api/gameroom/")
+      .then(res => res.json())
+      .then(data => {
+
+      })
+
+  }
+
+
 
   useEffect(() => {
     sendWelcome()
@@ -64,18 +90,20 @@ export default function Chat({ gameRoomID }: Props) {
 
       <br />
 
-      <ul id="chatBox" style={{ listStyleType: "none", border: "1px solid white", textAlign: "left" }}>
+      <ul id="chatBox" style={{ display: "flex", flexDirection: "column", listStyleType: "none", border: "1px solid white", overflowY: "scroll", height: "200px", padding: "10px", }}>
 
-        {listOfMessages.map((message: any, index: any) => (
-          <li key={index}>{message}</li>
-        ))}
-
+        {listOfMessages.map((message: any, index: any) => {
+          if (message.sender === localStorage.getItem("username")) {
+            return <li key={index} style={{ textAlign: "right" }}>{message.content}</li>;
+          }
+          return <li key={index} style={{ textAlign: "left" }}>{message.sender}: {message.content}</li>;
+        })}
 
       </ul>
       <div style={{ display: "flex" }}>
-        <form  >
-        <input type="text" style={{ width: "100%", height: "30px" }} name="messageInput" /> <button type="submit">Skicka</button>
-        </form>
+
+        {/* Vi måste hitta ett bättre sätt att göra dettta? liksom va fan är de här för knapp lol*/}
+        <input type="text" id="messageInput" /> <button onClick={() => { sendMessage((document.getElementById("messageInput") as HTMLInputElement).value); (document.getElementById("messageInput") as HTMLInputElement).value = ""; }}>Skicka</button>
       </div>
     </div>
   );
